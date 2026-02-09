049ce6e45278e19c25cc281ffe4a01368094ac23
abhishek <abhishekshah546@gmail.com>
2026-02-09 16:33:59 +0300

    admin rights

--- menu.py
+++ menu.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 def menu():
-    st.sidebar.image("logo.png", use_container_width =True)  # Adjust the path as needed
-    
+    st.sidebar.image("logo.png", use_container_width=True)
+
     st.sidebar.page_link("main.py", label="Dashboard")
     st.sidebar.page_link("pages/1_Upload_Lease.py", label="Upload Lease")
     st.sidebar.page_link("pages/2_Create_Client.py", label="Create Client")
@@ -11,8 +11,16 @@ def menu():
     st.sidebar.page_link("pages/5_Assistant.py", label="Assistant")
     st.sidebar.page_link("pages/6_Find_All_Leases.py", label="Find All Leases")
     st.sidebar.page_link("pages/7_Closed_Leases.py", label="Closed Leases")
-    st.sidebar.page_link("pages/8_Create_User.py", label="Create User")
+
+    # ✅ SUPER USER CHECK
+    if st.session_state.get("username") == "admin":
+        st.sidebar.markdown("---")
+        st.sidebar.page_link("pages/8_Create_User.py", label="Create User")
+        st.sidebar.page_link("pages/10_Edit_User.py", label="Edit Users")
+
+    st.sidebar.markdown("---")
+
     if st.sidebar.button("Logout"):
         st.session_state.clear()
         st.success("Logged out successfully!")
-        st.switch_page("pages/login.py")  # Redirect to login page
+        st.switch_page("pages/login.py")
